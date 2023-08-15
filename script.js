7bb5ca988ff9006c10f961d14a4e4423168d9d65
Byonee10 <65749103+Byonee10@users.noreply.github.com>
2023-08-16 00:14:56 +0300

    Added changing background color when hovering

--- script.js
+++ script.js
@@ -2,4 +2,12 @@ var innerHTMLString = "";
 for(let i = 0; i<16*16;i++){
     innerHTMLString += '<div class="box"></div>';
 }
-document.querySelector(".container").innerHTML = innerHTMLString
+document.querySelector(".container").innerHTML = innerHTMLString;
+
+const box = document.querySelectorAll(".box");
+
+function changeBackground(e){
+    e.target.style.backgroundColor = "black";
+}
+
+box.forEach(element => element.addEventListener("mouseover",changeBackground));
