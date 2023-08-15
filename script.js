da49e85db6a1735bffc5ac5c19d16705e66e2799
Byonee10 <65749103+Byonee10@users.noreply.github.com>
2023-08-16 00:29:15 +0300

    added reset button

--- script.js
+++ script.js
@@ -7,7 +7,21 @@ document.querySelector(".container").innerHTML = innerHTMLString;
 const box = document.querySelectorAll(".box");
 
 function changeBackground(e){
-    e.target.style.backgroundColor = "black";
+    e.target.classList.add("colored");
 }
 
 box.forEach(element => element.addEventListener("mouseover",changeBackground));
+
+const resetButton = document.createElement("button");
+resetButton.className = "res-button";
+const body = document.querySelector("body");
+resetButton.innerText = "Reset";
+body.appendChild(resetButton);
+
+function reset(){
+    box.forEach(element => {
+        element.classList.remove("colored");
+    } 
+    });
+resetButton.addEventListener("click",reset);
+
