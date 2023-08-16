402ebebb28e5fbc69227b24e682225c02eace2d4
Byonee10 <65749103+Byonee10@users.noreply.github.com>
2023-08-16 03:15:24 +0300

    Added eraser

--- script.js
+++ script.js
@@ -5,9 +5,13 @@ for(let i = 0; i<16*16;i++){
 document.querySelector(".container").innerHTML = innerHTMLString;
 
 const box = document.querySelectorAll(".box");
-
+let erasingMode = false;
 function changeBackground(e){
-    e.target.classList.add("colored");
+    if(!erasingMode){
+    e.target.classList.add("colored");}
+    else{
+        e.target.classList.remove("colored");
+    }
 }
 
 box.forEach(element => element.addEventListener("mouseover",changeBackground));
@@ -16,12 +20,23 @@ const resetButton = document.createElement("button");
 resetButton.className = "res-button";
 const body = document.querySelector("body");
 resetButton.innerText = "Reset";
-body.appendChild(resetButton);
+
 
 function reset(){
     box.forEach(element => {
         element.classList.remove("colored");
-    } 
-    });
+    })};
 resetButton.addEventListener("click",reset);
 
+const buttonContainer = document.createElement("div");
+buttonContainer.className = "btn-container";
+const eraser = document.createElement("button");
+eraser.innerText = "Eraser";
+buttonContainer.appendChild(resetButton);
+buttonContainer.appendChild(eraser);
+body.appendChild(buttonContainer);
+
+function erase(){
+    erasingMode = !erasingMode;
+}
+eraser.addEventListener("click",erase)
